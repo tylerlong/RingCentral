038c7061e8aa57ac44da19811c105c9e22b0c186
Tyler Long <tyler4long@gmail.com>
2016-07-10 23:03:37 +0800

    Finish country

---
+++ RingCentral.Test/CountryTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace RingCentral.Test
+{
+    [TestFixture]
+    public class CountryTest : BaseTest
+    {
+        [Test]
+        public void TestCase()
+        {
+            var country = rc.Get<Country>("/restapi/v1.0/dictionary/country/46").Result;
+            Assert.NotNull(country);
+            Assert.AreEqual("China", country.name);
+
+            var country2 = rc.RestApi().Dictionary().Country("46").Get().Result;
+            DeepEqual(country, country2);
+        }
+
+        [Test]
+        public void TestCase2()
+        {
+            var countries = rc.Get<Countries>("/restapi/v1.0/dictionary/country").Result;
+            Assert.NotNull(countries);
+            Assert.Greater(countries.records.Length, 1);
+
+            var countries2 = rc.RestApi().Dictionary().Country().List().Result;
+            DeepEqual(countries, countries2);
+        }
+    }
+}
+
---
+++ RingCentral/Models/Country.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+namespace RingCentral
+{
+    public partial class Country : Model
+    {
+        public Task<Country> Get(object queryParameters = null)
+        {
+            return Get<Country>(queryParameters);
+        }
+
+        public Task<Countries> List(object queryParameters = null)
+        {
+            return List<Countries>(queryParameters);
+        }
+    }
+
+    public static class CountryExtensions
+    {
+        public static Country Country(this Dictionary dictionary, string _id = null)
+        {
+            return new Country(dictionary, _id);
+        }
+    }
+}
