067f7441d10a9b2de2f3f246481a58ba31dc49ca
Tyler Long <tyler4long@gmail.com>
2016-07-11 23:47:19 +0800

    Refactor Http code

--- RingCentral/Http.cs
+++ RingCentral/Http.cs
@@ -8,7 +8,7 @@ namespace RingCentral
 {
     public partial class RestClient
     {
-        public Task<HttpResponseMessage> Get(string endpoint, object queryParams = null)
+        private FlurlClient GetClient(string endpoint, object queryParams)
         {
             var url = server.AppendPathSegment(endpoint).SetQueryParams(queryParams);
             var client = new FlurlClient(url);
@@ -16,29 +16,22 @@ namespace RingCentral
             {
                 client = client.WithOAuthBearerToken(token.access_token);
             }
-            return client.GetAsync();
+            return client;
+        }
+
+        public Task<HttpResponseMessage> Get(string endpoint, object queryParams = null)
+        {
+            return GetClient(endpoint, queryParams).GetAsync();
         }
 
         public Task<HttpResponseMessage> Post(string endpoint, object requestBody, object queryParams = null)
         {
-            var url = server.AppendPathSegment(endpoint).SetQueryParams(queryParams);
-            var client = new FlurlClient(url);
-            if (token != null)
-            {
-                client = client.WithOAuthBearerToken(token.access_token);
-            }
-            return client.PostJsonAsync(requestBody);
+            return GetClient(endpoint, queryParams).PostJsonAsync(requestBody);
         }
 
-        public Task<HttpResponseMessage> Delete(string endpoint)
+        public Task<HttpResponseMessage> Delete(string endpoint, object queryParams = null)
         {
-            var url = server.AppendPathSegment(endpoint);
-            var client = new FlurlClient(url);
-            if (token != null)
-            {
-                client = client.WithOAuthBearerToken(token.access_token);
-            }
-            return client.DeleteAsync();
+            return GetClient(endpoint, queryParams).DeleteAsync();
         }
 
         public async Task<T> Get<T>(string endpoint, object queryParams = null)
