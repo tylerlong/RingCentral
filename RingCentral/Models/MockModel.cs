711bf08d7bac66ae207a5ee0d3bef5735f3690e5
Tyler Long <tyler4long@gmail.com>
2016-07-11 10:59:14 +0800

    Add MockModel

---
+++ RingCentral/Models/MockModel.cs
@@ -0,0 +1,35 @@
+﻿namespace RingCentral
+{
+    public class MockModel : Model
+    {
+        public MockModel(RestClient rc) : base(null)
+        {
+            this.rc = rc;
+        }
+
+        protected override string PathSegment
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        protected override string Endpoint
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        private RestClient rc;
+        protected override RestClient RestClient
+        {
+            get
+            {
+                return rc;
+            }
+        }
+    }
+}
--- RingCentral/Models/RestApi.cs
+++ RingCentral/Models/RestApi.cs
@@ -1,35 +1,12 @@
 ﻿using System.Threading.Tasks;
-using Flurl;
 
 namespace RingCentral
 {
     public partial class RestApi
     {
-        protected override string Endpoint
-        {
-            get
-            {
-                return PathSegment.AppendPathSegment(_id).Path;
-            }
-        }
-
-        private RestClient rc;
-        protected override RestClient RestClient
-        {
-            get
-            {
-                return rc;
-            }
-        }
-
-        internal RestApi(RestClient rc, string _id = "v1.0") : base(null, _id)
-        {
-            this.rc = rc;
-        }
-
         public Task<RestApis> List(object queryParams = null)
         {
-            return RestClient.Get<RestApis>(PathSegment, queryParams);
+            return List<RestApis>(queryParams);
         }
     }
 
@@ -37,7 +14,7 @@ namespace RingCentral
     {
         public static RestApi RestApi(this RestClient rc, string uriString = "v1.0")
         {
-            var restApi = new RestApi(rc, uriString);
+            var restApi = new RestApi(new MockModel(rc), uriString);
             return restApi;
         }
     }
